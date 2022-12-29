9021b8ffc3584401de9b8506bc8af59daa55073b
LENOVO <giaopham16092001@gmail.com>
2022-12-29 23:45:27 +0700

    authentication

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,17 +1,21 @@
-// import "./bootstrap";
+import './bootstrap';
+import '../css/app.css';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
-import App from "./src/App";
-// import "../css/app.css";
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-ReactDOM.createRoot(document.getElementById("app")).render(
-    <Router>
-        <App />
-    </Router>
-);
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
